77171e5434c396cff5bca1df5b4b23de455a3ad3
Quijano <angel.q.nolasco@gmail.com>
2020-11-19 12:08:55 -0600

    Actualizacion subida de imagen

--- backend/routers/uploadRouter.js
+++ backend/routers/uploadRouter.js
@@ -6,7 +6,7 @@ const uploadRouter = express.Router();
 
 const storage = multer.diskStorage({
     destination(req, file, cb){
-        cb(null, 'uploads/');
+        cb(null, 'frontend/uploads/');
     },
     filename(req, file, cb){
         cb(null, `${Date.now()}.jpg`);
